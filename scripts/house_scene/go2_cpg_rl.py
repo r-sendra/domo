e8deee2425d50ac277ee897639a432c99f4349ae
r-sendra <r.sendra@alumnos.upm.es>
2026-06-23 10:27:39 +0200

    Train randomization of vy and vyaw

--- scripts/house_scene/go2_cpg_rl.py
+++ scripts/house_scene/go2_cpg_rl.py
@@ -187,7 +187,7 @@ class Go2CPGEnv:
         # DEFAULT = forward only (clean gait first). For omnidirectional
         # (paper Table III) widen to x:[-1,1] y:[-1,1] yaw:[-1,1].
         self.command_cfg = {
-            "lin_vel_x_range": [0.6, 2],
+            "lin_vel_x_range": [-0.5, 2],
             "lin_vel_y_range": [-0.5, 0.5],
             "ang_vel_range":   [-1.0, 1.0],
         }
